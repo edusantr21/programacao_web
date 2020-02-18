7685b10bd12dd12824b05ae2763e68af7169ca66
edusantr21 <edusantr21@gmail.com>
2020-02-17 21:18:06 -0300

    desafio 1

---
+++ HTML/JS/desafio1.js
@@ -0,0 +1,38 @@
+var myvar
+undefined
+myvar = 10
+10
+soma = 15 + 8
+23
+soma ++ 
+23
+soma * 3
+72
+// por enquanto o valor da variavel soma ate este exato momento é: 72
+undefined
+var souninja
+undefined
+souninja = true
+true
+var comida
+undefined
+comida = ["arroz", "feijão" , "ovo"]
+(3) ["arroz", "feijão", "ovo"]0: "arroz"1: "feijão"2: "ovo"length: 3__proto__: Array(0)
+comida [1]
+"feijão"
+soma == myvar
+false
+myvar <= soma
+true
+function funcaoDiv (num1, num2){
+if (num1 >= num2) }
+return (num1 / num2
+VM2402:2 Uncaught SyntaxError: Unexpected token '}'
+function funcaoDiv (num1, num2){
+if (num1 >= num2) 
+return (num1 / num2) }
+undefined
+funcaoDiv (5,2)
+2.5
+funcaoDiv (10,2)
+5
