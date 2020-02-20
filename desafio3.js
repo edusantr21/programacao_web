808a7282d2dd900036fdd9212446a5a548778199
edusantr21 <edusantr21@gmail.com>
2020-02-19 21:35:22 -0300

    Desafio 3

---
+++ desafio3.js
@@ -0,0 +1,38 @@
+Desafio 03
+Turma: 66641
+Aluno: Eduardo Santos
+Matéria: Programação Web
+
+1º)R=
+
+var x = {}
+_________________________________________________
+2º)R=
+
+var pessoa = { //objeto
+            nome: "Eduardo",
+            sobrenome: "Rodrigues",
+            sexo: "Masculino",
+            idade: 20,
+            altura: 1.79,
+            peso: 65,
+            andando: false,
+            caminhouQuantosMetros: 0,
+_________________________________________________
+3º)R=
+
+var pessoa = { //objeto
+            nome: "Eduardo",
+            sobrenome: "Rodrigues",
+            sexo: "Masculino",
+            idade: 20,
+            altura: 1.79,
+            peso: 65,
+            andando: false,
+            caminhouQuantosMetros: 0,
+fazerAniversário : function (){ //metodo
+    console.log(this.idade ++)
+    }
+}
+_________________________________________________
+4º)R=
